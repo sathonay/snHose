4430f5699382915485a27c33c62dd245a5898a5b
sathonay <support@newpaper.com>
2019-07-21 18:35:41 +0200

    Fix PotionType (private -> public)

--- snHose-API/src/main/java/com/github/snhose/utils/PotionType.java
+++ snHose-API/src/main/java/com/github/snhose/utils/PotionType.java
@@ -2,71 +2,71 @@ package com.github.snhose.utils;
 
 public class PotionType {
 
-    private static final short REGENERATION = 8193;
-    private static final short REGENERATION_II = 8225;
-    private static final short REGENERATION_EXTENDED = 8257;
-    private static final short REGENERATION_SPLASH = 16385;
-    private static final short REGENERATION_SPLASH_II = 16417;
-    private static final short REGENERATION_SPLASH_EXTENDED  = 16449;
+    public static final short REGENERATION = 8193;
+    public static final short REGENERATION_II = 8225;
+    public static final short REGENERATION_EXTENDED = 8257;
+    public static final short REGENERATION_SPLASH = 16385;
+    public static final short REGENERATION_SPLASH_II = 16417;
+    public static final short REGENERATION_SPLASH_EXTENDED  = 16449;
 
-    private static final short SPEED = 8194;
-    private static final short SPEED_II = 8226;
-    private static final short SPEED_EXTENDED = 8258;
-    private static final short SPEED_SPLASH = 16386;
-    private static final short SPEED_SPLASH_II = 16418;
-    private static final short SPEED_SPLASH_EXTENDED  = 16450;
+    public static final short SPEED = 8194;
+    public static final short SPEED_II = 8226;
+    public static final short SPEED_EXTENDED = 8258;
+    public static final short SPEED_SPLASH = 16386;
+    public static final short SPEED_SPLASH_II = 16418;
+    public static final short SPEED_SPLASH_EXTENDED  = 16450;
     
-    private static final short FIRE_RESISTANCE = 8227;
-    private static final short FIRE_RESISTANCE_EXTENDED = 8259;
-    private static final short FIRE_RESISTANCE_SPLASH = 16429;
-    private static final short FIRE_RESISTANCE_SPLASH_EXTENDED  = 16451;
+    public static final short FIRE_RESISTANCE = 8227;
+    public static final short FIRE_RESISTANCE_EXTENDED = 8259;
+    public static final short FIRE_RESISTANCE_SPLASH = 16429;
+    public static final short FIRE_RESISTANCE_SPLASH_EXTENDED  = 16451;
 
-    private static final short POISON = 8196;
-    private static final short POISON_II = 8228;
-    private static final short POISON_EXTENDED = 8260;
-    private static final short POISON_SPLASH = 16388;
-    private static final short POISON_SPLASH_II = 16420;
-    private static final short POISON_SPLASH_EXTENDED  = 16452;
+    public static final short POISON = 8196;
+    public static final short POISON_II = 8228;
+    public static final short POISON_EXTENDED = 8260;
+    public static final short POISON_SPLASH = 16388;
+    public static final short POISON_SPLASH_II = 16420;
+    public static final short POISON_SPLASH_EXTENDED  = 16452;
 
-    private static final short INSTANT_HEALTH = 8261;
-    private static final short INSTANT_HEALTH_II = 8229;
-    private static final short INSTANT_HEALTH_SPLASH = 16453;
-    private static final short INSTANT_HEALTH_SPLASH_II = 16421;
+    public static final short INSTANT_HEALTH = 8261;
+    public static final short INSTANT_HEALTH_II = 8229;
+    public static final short INSTANT_HEALTH_SPLASH = 16453;
+    public static final short INSTANT_HEALTH_SPLASH_II = 16421;
 
-    private static final short NIGHT_VISION = 8230;
-    private static final short NIGHT_VISION_EXTENDED  = 8262;
-    private static final short NIGHT_VISION_SPLASH = 16422;
-    private static final short NIGHT_VISION_SPLASH_EXTENDED  = 16454;
+    public static final short NIGHT_VISION = 8230;
+    public static final short NIGHT_VISION_EXTENDED  = 8262;
+    public static final short NIGHT_VISION_SPLASH = 16422;
+    public static final short NIGHT_VISION_SPLASH_EXTENDED  = 16454;
 
-    private static final short WEAKNESS = 8232;
-    private static final short WEAKNESS_EXTENDED  = 8264;
-    private static final short WEAKNESS_SPLASH = 16424;
-    private static final short WEAKNESS_SPLASH_EXTENDED  = 16456;
+    public static final short WEAKNESS = 8232;
+    public static final short WEAKNESS_EXTENDED  = 8264;
+    public static final short WEAKNESS_SPLASH = 16424;
+    public static final short WEAKNESS_SPLASH_EXTENDED  = 16456;
 
-    private static final short STRENGTH = 8201;
-    private static final short STRENGTH_II = 8233;
-    private static final short STRENGTH_EXTENDED = 8265;
-    private static final short STRENGTH_SPLASH = 16393;
-    private static final short STRENGTH_SPLASH_II = 16425;
-    private static final short STRENGTH_SPLASH_EXTENDED  = 16427;
+    public static final short STRENGTH = 8201;
+    public static final short STRENGTH_II = 8233;
+    public static final short STRENGTH_EXTENDED = 8265;
+    public static final short STRENGTH_SPLASH = 16393;
+    public static final short STRENGTH_SPLASH_II = 16425;
+    public static final short STRENGTH_SPLASH_EXTENDED  = 16427;
 
-    private static final short SLOWNESS = 8234;
-    private static final short SLOWNESS_EXTENDED  = 8266;
-    private static final short SLOWNESS_SPLASH = 16426;
-    private static final short SLOWNESS_SPLASH_EXTENDED  = 16458;
+    public static final short SLOWNESS = 8234;
+    public static final short SLOWNESS_EXTENDED  = 8266;
+    public static final short SLOWNESS_SPLASH = 16426;
+    public static final short SLOWNESS_SPLASH_EXTENDED  = 16458;
     
-    private static final short INSTANT_DAMAGE = 8268;
-    private static final short INSTANT_DAMAGE_EXTENDED  = 8236;
-    private static final short INSTANT_DAMAGE_SPLASH = 16460;
-    private static final short INSTANT_DAMAGE_SPLASH_EXTENDED  = 16428;
+    public static final short INSTANT_DAMAGE = 8268;
+    public static final short INSTANT_DAMAGE_EXTENDED  = 8236;
+    public static final short INSTANT_DAMAGE_SPLASH = 16460;
+    public static final short INSTANT_DAMAGE_SPLASH_EXTENDED  = 16428;
 
-    private static final short WATER_BREATHING = 8237;
-    private static final short WATER_BREATHING_EXTENDED  = 8269;
-    private static final short WATER_BREATHING_SPLASH = 16429;
-    private static final short WATER_BREATHING_SPLASH_EXTENDED  = 16461;
+    public static final short WATER_BREATHING = 8237;
+    public static final short WATER_BREATHING_EXTENDED  = 8269;
+    public static final short WATER_BREATHING_SPLASH = 16429;
+    public static final short WATER_BREATHING_SPLASH_EXTENDED  = 16461;
 
-    private static final short INVISIBILITY = 8238;
-    private static final short INVISIBILITY_EXTENDED  = 8270;
-    private static final short INVISIBILITY_SPLASH = 16430;
-    private static final short INVISIBILITY_SPLASH_EXTENDED  = 16462;
+    public static final short INVISIBILITY = 8238;
+    public static final short INVISIBILITY_EXTENDED  = 8270;
+    public static final short INVISIBILITY_SPLASH = 16430;
+    public static final short INVISIBILITY_SPLASH_EXTENDED  = 16462;
 }
