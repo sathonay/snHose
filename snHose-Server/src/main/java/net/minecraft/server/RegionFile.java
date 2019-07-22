cb6a9e963fec635dfbd6c205ee55285e1e66fb2b
Noksio <42706907+NoksBlue@users.noreply.github.com>
2019-07-21 20:22:46 -0400

    Fix things

--- snHose-Server/src/main/java/net/minecraft/server/RegionFile.java
+++ snHose-Server/src/main/java/net/minecraft/server/RegionFile.java
@@ -171,7 +171,7 @@ public class RegionFile {
     }
 
     public DataOutputStream b(int i, int j) {
-        return this.d(i, j) ? null : new DataOutputStream(new DeflaterOutputStream(new ChunkBuffer(this, i, j)));
+        return this.d(i, j) ? null : new DataOutputStream(new java.io.BufferedOutputStream(new DeflaterOutputStream(new ChunkBuffer(this, i, j))));
     }
 
     protected synchronized void a(int i, int j, byte[] abyte, int k) {
