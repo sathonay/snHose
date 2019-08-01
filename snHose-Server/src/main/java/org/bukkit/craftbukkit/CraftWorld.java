dd2681657c6d819c81a9ec3975ebedbc7afa5a86
Noksio <42706907+NoksBlue@users.noreply.github.com>
2019-08-01 13:42:09 -0400

    Put owner on drop

--- snHose-Server/src/main/java/org/bukkit/craftbukkit/CraftWorld.java
+++ snHose-Server/src/main/java/org/bukkit/craftbukkit/CraftWorld.java
@@ -317,17 +317,22 @@ public class CraftWorld implements World {
     }
 
     public org.bukkit.entity.Item dropItem(Location loc, ItemStack item) {
+        return dropItem(null, loc, item);
+    }
+    
+    public org.bukkit.entity.Item dropItem(EntityHuman owner, Location loc, ItemStack item) {
         Validate.notNull(item, "Cannot drop a Null item.");
         Validate.isTrue(item.getTypeId() != 0, "Cannot drop AIR.");
         EntityItem entity = new EntityItem(world, loc.getX(), loc.getY(), loc.getZ(), CraftItemStack.asNMSCopy(item));
         entity.pickupDelay = 10;
+        entity.owner = owner;
         world.addEntity(entity);
         // TODO this is inconsistent with how Entity.getBukkitEntity() works.
         // However, this entity is not at the moment backed by a server entity class so it may be left.
         return new CraftItem(world.getServer(), entity);
     }
-
-    public org.bukkit.entity.Item dropItemNaturally(Location loc, ItemStack item) {
+    
+    public org.bukkit.entity.Item dropItemNaturally(EntityHuman owner, Location loc, ItemStack item) {
         double xs = world.random.nextFloat() * 0.7F + (1.0F - 0.7F) * 0.5D;
         double ys = world.random.nextFloat() * 0.7F + (1.0F - 0.7F) * 0.5D;
         double zs = world.random.nextFloat() * 0.7F + (1.0F - 0.7F) * 0.5D;
@@ -335,7 +340,11 @@ public class CraftWorld implements World {
         loc.setX(loc.getX() + xs);
         loc.setY(loc.getY() + ys);
         loc.setZ(loc.getZ() + zs);
-        return dropItem(loc, item);
+        return dropItem(owner, loc, item);
+    }
+
+    public org.bukkit.entity.Item dropItemNaturally(Location loc, ItemStack item) {
+        return dropItemNaturally(null, loc, item);
     }
 
     public Arrow spawnArrow(Location loc, Vector velocity, float speed, float spread) {
