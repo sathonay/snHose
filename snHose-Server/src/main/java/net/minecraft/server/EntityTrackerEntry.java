71525cc0e7af7938c83d46fd4aa5e03b242b1c94
sathonay <sathonaypro@gmail.com>
2019-08-03 10:37:59 +0200

    Update EntityTrackerEntry.java

--- snHose-Server/src/main/java/net/minecraft/server/EntityTrackerEntry.java
+++ snHose-Server/src/main/java/net/minecraft/server/EntityTrackerEntry.java
@@ -1,12 +1,7 @@
 package net.minecraft.server;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
-import com.google.common.collect.Sets;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -146,7 +141,7 @@ public class EntityTrackerEntry {
                         this.v = 0;
                         // CraftBukkit start - Refresh list of who can see a player before sending teleport packet
                         if (this.tracker instanceof EntityPlayer) {
-                            this.scanPlayers(new java.util.ArrayList(this.trackedPlayers));
+                            this.scanPlayers(new ArrayList<>(this.trackedPlayers));
                         }
                         // CraftBukkit end
                         packet = new PacketPlayOutEntityTeleport(this.tracker.getId(), i, j, k, (byte) l, (byte) i1, tracker.onGround, tracker instanceof EntityFallingBlock || tracker instanceof EntityTNTPrimed); // Spigot - protocol patch
