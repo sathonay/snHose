3d7827d05023dce8f2b6bdf462ec203c50fcfa4d
Noksio <42706907+NoksBlue@users.noreply.github.com>
2019-08-08 13:34:09 -0400

    Update EntityChicken.java

--- snHose-Server/src/main/java/net/minecraft/server/EntityChicken.java
+++ snHose-Server/src/main/java/net/minecraft/server/EntityChicken.java
@@ -97,12 +97,7 @@ public class EntityChicken extends EntityAnimal {
         for (int k = 0; k < j; ++k) {
             this.a(Items.FEATHER, 1);
         }
-
-        if (this.isBurning()) {
-            this.a(Items.COOKED_CHICKEN, 1);
-        } else {
-            this.a(Items.RAW_CHICKEN, 1);
-        }
+        this.a((this.isBurning() ? Items.COOKED_CHICKEN : Items.RAW_CHICKEN), 1);
     }
 
     public EntityChicken b(EntityAgeable entityageable) {
