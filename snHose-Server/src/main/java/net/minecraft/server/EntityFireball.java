48db1bd3be484e21dd509de670ee941a6040687f
Noksio <42706907+NoksBlue@users.noreply.github.com>
2019-08-08 13:20:00 -0400

    Update EntityFireball.java

--- snHose-Server/src/main/java/net/minecraft/server/EntityFireball.java
+++ snHose-Server/src/main/java/net/minecraft/server/EntityFireball.java
@@ -104,8 +104,9 @@ public abstract class EntityFireball extends Entity {
             List list = this.world.getEntities(this, this.boundingBox.a(this.motX, this.motY, this.motZ).grow(1.0D, 1.0D, 1.0D));
             double d0 = 0.0D;
 
-            for (int i = 0; i < list.size(); ++i) {
-                Entity entity1 = (Entity) list.get(i);
+            Iterator<Entity> iterator = this.world.getEntities(this, this.boundingBox.a(this.motX, this.motY, this.motZ).grow(1.0D, 1.0D, 1.0D)).iterator();
+            while (iterator.hasNext()){
+                Entity entity1 = iterator.next();
 
                 if (entity1.R() && (!entity1.i(this.shooter) || this.au >= 25)) {
                     float f = 0.3F;
