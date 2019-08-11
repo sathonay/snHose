45558e8567d80c124047ee60f361234887f1f44f
Noksio <42706907+NoksBlue@users.noreply.github.com>
2019-08-11 12:06:07 -0400

    Update ServerEvent.java

--- snHose-API/src/main/java/org/bukkit/event/server/ServerEvent.java
+++ snHose-API/src/main/java/org/bukkit/event/server/ServerEvent.java
@@ -1,9 +1,17 @@
 package org.bukkit.event.server;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.Event;
 
 /**
  * Miscellaneous server events
  */
 public abstract class ServerEvent extends Event {
+    public ServerEvent(boolean isAsync) {
+        super(isAsync);
+    }
+
+    public ServerEvent() {
+        super(!Bukkit.isPrimaryThread());
+    }
 }
