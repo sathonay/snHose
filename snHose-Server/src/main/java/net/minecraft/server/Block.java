c4bb0039fbae9d3cb2523f166775c693155185df
sathonay <sathonaypro@gmail.com>
2019-08-13 21:22:29 +0200

    Update Block.java

--- snHose-Server/src/main/java/net/minecraft/server/Block.java
+++ snHose-Server/src/main/java/net/minecraft/server/Block.java
@@ -603,15 +603,15 @@ public class Block {
     }
 
     private boolean a(Vec3D vec3d) {
-        return vec3d == null ? false : vec3d.b >= this.minY && vec3d.b <= this.maxY && vec3d.c >= this.minZ && vec3d.c <= this.maxZ;
+        return vec3d != null && (vec3d.b >= this.minY && vec3d.b <= this.maxY && vec3d.c >= this.minZ && vec3d.c <= this.maxZ);
     }
 
     private boolean b(Vec3D vec3d) {
-        return vec3d == null ? false : vec3d.a >= this.minX && vec3d.a <= this.maxX && vec3d.c >= this.minZ && vec3d.c <= this.maxZ;
+        return vec3d != null && (vec3d.a >= this.minX && vec3d.a <= this.maxX && vec3d.c >= this.minZ && vec3d.c <= this.maxZ);
     }
 
     private boolean c(Vec3D vec3d) {
-        return vec3d == null ? false : vec3d.a >= this.minX && vec3d.a <= this.maxX && vec3d.b >= this.minY && vec3d.b <= this.maxY;
+        return vec3d != null && (vec3d.a >= this.minX && vec3d.a <= this.maxX && vec3d.b >= this.minY && vec3d.b <= this.maxY);
     }
 
     public void wasExploded(World world, int i, int j, int k, Explosion explosion) {}
@@ -787,7 +787,7 @@ public class Block {
     }
 
     public static boolean a(Block block, Block block1) {
-        return block != null && block1 != null ? (block == block1 ? true : block.c(block1)) : false;
+        return (block != null && block1 != null) && (block == block1 || block.c(block1));
     }
 
     public boolean isComplexRedstone() {
@@ -814,10 +814,7 @@ public class Block {
         if (value < min) {
             return min;
         }
-        if (value > max) {
-            return max;
-        }
-        return value;
+        return Math.min(value, max);
     }
     // Spigot end
 }
