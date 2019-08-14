d6ba0ae2cf26454f47cc229879bf04788713aa9a
Noksio <42706907+NoksBlue@users.noreply.github.com>
2019-08-14 18:19:56 -0400

    Update EntityHuman.java

--- snHose-Server/src/main/java/net/minecraft/server/EntityHuman.java
+++ snHose-Server/src/main/java/net/minecraft/server/EntityHuman.java
@@ -901,8 +901,6 @@ public abstract class EntityHuman extends EntityLiving implements ICommandListen
     public double ad() {
         return (double) (this.height - 0.5F);
     }
-    
-    private final net.minecraft.server.Knockback knockback = new net.minecraft.server.Knockback();
 
     public void attack(Entity entity) {
         if (entity.av()) {
@@ -949,7 +947,7 @@ public abstract class EntityHuman extends EntityLiving implements ICommandListen
                     final boolean flag2 = entity.damageEntity(DamageSource.playerAttack(this), f);
                     if (flag2) {
                         if (i > 0) {
-                            entity.g((-Math.sin(this.yaw * Math.PI / 180.0F) * (float) i * knockback.getSprintForce()), knockback.getSprintHeight() / knockback.getFriction() /* Try to adjust with friction */, (Math.cos(this.yaw * Math.PI / 180.0F) * i * knockback.getSprintHeight()));
+                            entity.g((-Math.sin(this.yaw * Math.PI / 180.0F) * (float) i * this.world.getServer().getKnockback().getSprintForce()), this.world.getServer().getKnockback().getSprintHeight() / this.world.getServer().getKnockback().getFriction() /* Try to adjust with friction */, (Math.cos(this.yaw * Math.PI / 180.0F) * i * this.world.getServer().getKnockback().getSprintHeight()));
                             this.motX *= 0.6D;
                             // NEVER PUT "this.motY *= 0.6D" only skid do that
                             this.motZ *= 0.6D;
