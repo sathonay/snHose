f067be36b8622ba449181ef7d1ccbe669b6bb5a5
Noksio <42706907+NoksBlue@users.noreply.github.com>
2019-08-14 14:10:44 -0400

    Update EnderpearlLandEvent.java

--- snHose-API/src/main/java/org/bukkit/event/entity/EnderpearlLandEvent.java
+++ snHose-API/src/main/java/org/bukkit/event/entity/EnderpearlLandEvent.java
@@ -1,19 +1,53 @@
 package org.bukkit.event.entity;
 
 import org.bukkit.entity.EnderPearl;
+import org.bukkit.entity.Entity;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 
-public class EnderpearlLandEvent extends EntityEvent {
+public class EnderpearlLandEvent extends EntityEvent implements Cancellable {
 
     private static final HandlerList handlers = new HandlerList();
+    private final Reason reason;
+    private final Entity hit;
+    private boolean cancel;
 
-    public EnderpearlLandEvent(final EnderPearl enderPearl) {
+    public EnderpearlLandEvent(final EnderPearl enderPearl, final Reason reason) {
+        this(enderPearl, reason, null);
+    }
+    
+    public EnderpearlLandEvent(final EnderPearl enderPearl, final Reason reason, final Entity hit) {
         super(enderPearl);
+        this.reason = reason;
+        this.hit = hit;
     }
 
     @Override
     public EnderPearl getEntity() {
         return (EnderPearl) entity;
+    }
+    
+    public Reason getReason() {
+        return reason;
+    }
+    
+    /**
+     * Returns the entity that was hit by the Enderpearl
+     *
+     * @return Entity hit
+     */
+    public Entity getHit() {
+        return hit;
+    }
+    
+    @Override
+    public boolean isCancelled() {
+        return cancel;
+    }
+
+    @Override
+    public void setCancelled(boolean cancel) {
+        this.cancel = cancel;
     }
 
     @Override
@@ -23,6 +57,17 @@ public class EnderpearlLandEvent extends EntityEvent {
 
     public static HandlerList getHandlerList() {
         return handlers;
+    }
+    
+    public enum Reason {
+        /**
+         * Called when the pearl hits a block
+         */
+        BLOCK,
+        /**
+         * Called when the pearl hits an entity
+         */
+        ENTITY
     }
 
 }
