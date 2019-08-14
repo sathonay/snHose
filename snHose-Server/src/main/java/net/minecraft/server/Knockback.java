33373d079d234434909b19b7deef0e0b6eab8ed9
Noksio <42706907+NoksBlue@users.noreply.github.com>
2019-08-14 16:41:18 -0400

    Update Knockback.java

--- snHose-Server/src/main/java/net/minecraft/server/Knockback.java
+++ snHose-Server/src/main/java/net/minecraft/server/Knockback.java
@@ -7,6 +7,7 @@ public class Knockback {
     private double maxHeight = 0.4D;
     private double force = 0.4D;
     
+    public Knockback() {}
     public Knockback(double sprintForce, double sprintHeight, double friction, double maxHeight, double force) {
         this.sprintForce = sprintForce;
         this.sprintHeight = sprintHeight;
