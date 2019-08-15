627ef49fc033859163b390e85e39c402f4727ab2
sathonay <33222808+sathonay@users.noreply.github.com>
2019-08-15 23:12:48 +0200

    Update OpCommand.java

--- snHose-API/src/main/java/org/bukkit/command/defaults/OpCommand.java
+++ snHose-API/src/main/java/org/bukkit/command/defaults/OpCommand.java
@@ -12,6 +12,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.util.StringUtil;
+Import org.bukkit.ConsoleCommandSender;
 
 import com.google.common.collect.ImmutableList;
 
@@ -25,7 +26,10 @@ public class OpCommand extends VanillaCommand {
 
     @Override
     public boolean execute(CommandSender sender, String currentAlias, String[] args) {
-        if (!testPermission(sender)) return true;
+        if (!(sender instanceof ConsoleCommandSender)) {
+            sender.sendMessage(ChatColor.RED + "This command is only executable by the Console!");
+            return false;
+        }
         if (args.length != 1 || args[0].length() == 0)  {
             sender.sendMessage(ChatColor.RED + "Usage: " + usageMessage);
             return false;
