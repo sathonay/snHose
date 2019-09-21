82fdb012cc77006b0d1efabbdb359ec8782729b1
sathonay <sathonaypro@gmail.com>
2019-09-21 19:48:03 +0200

    Update SlotHorseArmor.java

--- snHose-Server/src/main/java/net/minecraft/server/SlotHorseArmor.java
+++ snHose-Server/src/main/java/net/minecraft/server/SlotHorseArmor.java
@@ -6,9 +6,9 @@ class SlotHorseArmor extends Slot
     final /* synthetic */ ContainerHorse b;
     
     SlotHorseArmor(final ContainerHorse b, final IInventory iinventory, final int i, final int j, final int k, final EntityHorse a) {
+        super(iinventory, i, j, k);
         this.b = b;
         this.a = a;
-        super(iinventory, i, j, k);
     }
     
     @Override
