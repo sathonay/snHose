1b131548c9804aa027c9f98def0c35e8e306cbcc
sathonay <sathonaypro@gmail.com>
2019-09-21 23:30:50 +0200

    Update ScoreboardComparator.java

--- snHose-Server/src/main/java/net/minecraft/server/ScoreboardComparator.java
+++ snHose-Server/src/main/java/net/minecraft/server/ScoreboardComparator.java
@@ -2,7 +2,7 @@ package net.minecraft.server;
 
 import java.util.*;
 
-final class ScoreboardComparator implements Comparator
+final class ScoreboardComparator implements Comparator<ScoreboardScore>
 {
     public int a(final ScoreboardScore scoreboardScore, final ScoreboardScore scoreboardScore2) {
         if (scoreboardScore.getScore() > scoreboardScore2.getScore()) {
@@ -12,5 +12,10 @@ final class ScoreboardComparator implements Comparator
             return -1;
         }
         return 0;
+    }
+
+    @Override
+    public int compare(ScoreboardScore o1, ScoreboardScore o2) {
+        return this.a(o1, o2);
     }
 }
