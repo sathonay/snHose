30672d02f470e53c86d6f80fe9f3ac0de2ed5c05
sathonay <sathonaypro@gmail.com>
2019-09-21 19:11:32 +0200

    Update ChunkCoordinatesPortal.java

--- snHose-Server/src/main/java/net/minecraft/server/ChunkCoordinatesPortal.java
+++ snHose-Server/src/main/java/net/minecraft/server/ChunkCoordinatesPortal.java
@@ -6,8 +6,8 @@ public class ChunkCoordinatesPortal extends ChunkCoordinates
     final /* synthetic */ PortalTravelAgent e;
     
     public ChunkCoordinatesPortal(final PortalTravelAgent e, final int n, final int n2, final int n3, final long d) {
-        this.e = e;
         super(n, n2, n3);
+        this.e = e;
         this.d = d;
     }
 }
