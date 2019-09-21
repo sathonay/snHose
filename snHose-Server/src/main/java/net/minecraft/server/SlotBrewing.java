9d9300f8f991f138d7f7b7a496adea985971d387
sathonay <sathonaypro@gmail.com>
2019-09-21 19:47:55 +0200

    Update SlotBrewing.java

--- snHose-Server/src/main/java/net/minecraft/server/SlotBrewing.java
+++ snHose-Server/src/main/java/net/minecraft/server/SlotBrewing.java
@@ -5,8 +5,8 @@ class SlotBrewing extends Slot
     final /* synthetic */ ContainerBrewingStand a;
     
     public SlotBrewing(final ContainerBrewingStand a, final IInventory iinventory, final int i, final int j, final int k) {
-        this.a = a;
         super(iinventory, i, j, k);
+        this.a = a;
     }
     
     @Override
