6cec9140aa27d30c46254af9f66a8b6c2d89a7f6
sathonay <sathonaypro@gmail.com>
2019-09-21 19:47:59 +0200

    Update SlotEnchant.java

--- snHose-Server/src/main/java/net/minecraft/server/SlotEnchant.java
+++ snHose-Server/src/main/java/net/minecraft/server/SlotEnchant.java
@@ -5,8 +5,8 @@ class SlotEnchant extends Slot
     final /* synthetic */ ContainerEnchantTable a;
     
     SlotEnchant(final ContainerEnchantTable a, final IInventory iinventory, final int i, final int j, final int k) {
-        this.a = a;
         super(iinventory, i, j, k);
+        this.a = a;
     }
     
     @Override
