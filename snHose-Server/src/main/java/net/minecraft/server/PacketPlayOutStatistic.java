a8e1a843d7574b6d47d8d31f1494a026e18c7781
sathonay <sathonaypro@gmail.com>
2019-09-28 13:43:30 +0200

    Update PacketPlayOutStatistic.java

--- snHose-Server/src/main/java/net/minecraft/server/PacketPlayOutStatistic.java
+++ snHose-Server/src/main/java/net/minecraft/server/PacketPlayOutStatistic.java
@@ -1,11 +1,13 @@
 package net.minecraft.server;
 
 import net.minecraft.util.com.google.common.collect.*;
+
+import java.io.IOException;
 import java.util.*;
 
 public class PacketPlayOutStatistic extends Packet
 {
-    private Map a;
+    private Map<Statistic, Integer> a;
     
     public PacketPlayOutStatistic() {
     }
@@ -23,23 +25,38 @@ public class PacketPlayOutStatistic extends Packet
         final int a = packetDataSerializer.a();
         this.a = Maps.newHashMap();
         for (int i = 0; i < a; ++i) {
-            final Statistic statistic = StatisticList.getStatistic(packetDataSerializer.c(32767));
-            final int a2 = packetDataSerializer.a();
-            if (statistic != null) {
-                this.a.put(statistic, a2);
+            final Statistic statistic;
+            try {
+                statistic = StatisticList.getStatistic(packetDataSerializer.c(32767));
+                final int a2 = packetDataSerializer.a();
+                if (statistic != null) {
+                    this.a.put(statistic, a2);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+
         }
     }
     
     @Override
     public void b(final PacketDataSerializer packetDataSerializer) {
         packetDataSerializer.b(this.a.size());
-        for (final Map.Entry<Statistic, V> entry : this.a.entrySet()) {
-            packetDataSerializer.a(entry.getKey().name);
-            packetDataSerializer.b((int)entry.getValue());
+        for (final Map.Entry<Statistic, Integer> entry : this.a.entrySet()) {
+            try {
+                packetDataSerializer.a(entry.getKey().name);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            packetDataSerializer.b(entry.getValue());
         }
     }
-    
+
+    @Override
+    public void handle(PacketListener packetlistener) {
+        this.a((PacketPlayOutListener) packetlistener);
+    }
+
     @Override
     public String b() {
         return String.format("count=%d", this.a.size());
